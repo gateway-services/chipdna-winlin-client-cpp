1dfedc282c983480a6a933325603b81dc632a631
mbsws <matthew.burke@nmi.com>
2019-10-30 15:03:14 +0000

    Updated for ChipDNA 2.13 Aldur

--- CppLib/src/libraryversion.cpp
+++ CppLib/src/libraryversion.cpp
@@ -8,8 +8,8 @@ namespace ChipDnaClientLib {
 	const std::string LibraryVersion::ClientOsTag = "CLIENT_OPERATING_SYSTEM";
 	const std::string LibraryVersion::ClientTypeTag = "CLIENT_TYPE";
 
-	const std::string LibraryVersion::ClientLibraryReleaseName = "ERU ILUVATAR";
-	const std::string LibraryVersion::ClientLibraryVersion = "2.12.3593994";
+	const std::string LibraryVersion::ClientLibraryReleaseName = "ALDUR";
+	const std::string LibraryVersion::ClientLibraryVersion = "2.13.12127108";
 	const std::string LibraryVersion::ClientLibraryType = "C++";
 
 }
