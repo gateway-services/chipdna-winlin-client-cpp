32844f174431ac8f10c9b87006c073e31bab1646
cmsws <128141511+cmsws@users.noreply.github.com>
2023-12-05 09:45:42 +0000

    Updated for ChipDNA 3.13 Emrys

--- CppLib/src/libraryversion.cpp
+++ CppLib/src/libraryversion.cpp
@@ -8,8 +8,8 @@ namespace ChipDnaClientLib {
 	const std::string LibraryVersion::ClientOsTag = "CLIENT_OPERATING_SYSTEM";
 	const std::string LibraryVersion::ClientTypeTag = "CLIENT_TYPE";
 
-	const std::string LibraryVersion::ClientLibraryReleaseName = "MYRDDIN";
-	const std::string LibraryVersion::ClientLibraryVersion = "3.12.1344.7444";
+	const std::string LibraryVersion::ClientLibraryReleaseName = "EMRYS";
+	const std::string LibraryVersion::ClientLibraryVersion = "3.13.1422.6238";
 	const std::string LibraryVersion::ClientLibraryType = "C++";
 
 }
