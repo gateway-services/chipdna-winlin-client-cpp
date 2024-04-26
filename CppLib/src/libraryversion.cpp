a054df81f3f9325657064d960e07c51825fbe8b5
kljsws <144147680+kljsws@users.noreply.github.com>
2024-04-26 14:15:17 +0100

    Updated for ChipDNA 3.14 Phoenix

--- CppLib/src/libraryversion.cpp
+++ CppLib/src/libraryversion.cpp
@@ -8,8 +8,8 @@ namespace ChipDnaClientLib {
 	const std::string LibraryVersion::ClientOsTag = "CLIENT_OPERATING_SYSTEM";
 	const std::string LibraryVersion::ClientTypeTag = "CLIENT_TYPE";
 
-	const std::string LibraryVersion::ClientLibraryReleaseName = "EMRYS";
-	const std::string LibraryVersion::ClientLibraryVersion = "3.13.1422.6238";
+	const std::string LibraryVersion::ClientLibraryReleaseName = "PHOENIX";
+	const std::string LibraryVersion::ClientLibraryVersion = "3.14.1524.5421";
 	const std::string LibraryVersion::ClientLibraryType = "C++";
 
 }
