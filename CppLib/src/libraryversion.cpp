3e2a863c1d2971fc7b37441807f34c2e23dfd558
cmsws <128141511+cmsws@users.noreply.github.com>
2024-06-13 15:48:58 +0100

    Updated for ChipDNA 3.15 Impundulu

--- CppLib/src/libraryversion.cpp
+++ CppLib/src/libraryversion.cpp
@@ -8,8 +8,8 @@ namespace ChipDnaClientLib {
 	const std::string LibraryVersion::ClientOsTag = "CLIENT_OPERATING_SYSTEM";
 	const std::string LibraryVersion::ClientTypeTag = "CLIENT_TYPE";
 
-	const std::string LibraryVersion::ClientLibraryReleaseName = "PHOENIX";
-	const std::string LibraryVersion::ClientLibraryVersion = "3.14.1524.5421";
+	const std::string LibraryVersion::ClientLibraryReleaseName = "IMPUNDULU";
+	const std::string LibraryVersion::ClientLibraryVersion = "3.15.1574.5734";
 	const std::string LibraryVersion::ClientLibraryType = "C++";
 
 }
