2db406a76e9e2f58e4ef8b88f189201fed52bb85
cmsws <128141511+cmsws@users.noreply.github.com>
2025-08-29 16:08:14 +0100

    Updated for ChipDNA 3.19 Okami

--- CppLib/src/libraryversion.cpp
+++ CppLib/src/libraryversion.cpp
@@ -8,8 +8,8 @@ namespace ChipDnaClientLib {
 	const std::string LibraryVersion::ClientOsTag = "CLIENT_OPERATING_SYSTEM";
 	const std::string LibraryVersion::ClientTypeTag = "CLIENT_TYPE";
 
-	const std::string LibraryVersion::ClientLibraryReleaseName = "INARI";
-	const std::string LibraryVersion::ClientLibraryVersion = "3.18.1868.3188";
+	const std::string LibraryVersion::ClientLibraryReleaseName = "OKAMI";
+	const std::string LibraryVersion::ClientLibraryVersion = "3.19.1955.5536";
 	const std::string LibraryVersion::ClientLibraryType = "C++";
 
 }
